440adbe1bb0083189d9df75f0b9912d53cfdabd7
Joy119140036 <83533087+Joy119140036@users.noreply.github.com>
2022-06-10 10:10:30 +0700

    data

---
+++ Data.jsx
@@ -0,0 +1,44 @@
+const JADWAL = [
+    {
+        jadwal_id: "1",
+        dermaga_keberangkatan: "Merak",
+        dermaga_tujuan: "Bakauheni",
+        kelas_layanan: "Ekspress", 
+        tanggal_masuk: "Sabtu, 2 Maret 2022",
+        jam_masuk: "18.00 WIB"
+    },
+    {
+        jadwal_id: "2",
+        dermaga_keberangkatan: "Merak",
+        dermaga_tujuan: "Bakauheni",
+        kelas_layanan: "Express", 
+        tanggal_masuk: "Senin, 6 Maret 2022",
+        jam_masuk: "12.30 WIB"
+    },
+    {
+        jadwal_id: "3",
+        dermaga_keberangkatan: "Merak",
+        dermaga_tujuan: "Bakauheni",
+        kelas_layanan: "Reguler", 
+        tanggal_masuk: "Minggu, 20 Maret 2022",
+        jam_masuk: "11.30 WIB"
+    },
+    {
+        jadwal_id: "4",
+        dermaga_keberangkatan: "Bakauheni",
+        dermaga_tujuan: "Merak",
+        kelas_layanan: "Reguler", 
+        tanggal_masuk: "Minggu, 7 April 2022",
+        jam_masuk: "15.00 WIB"
+    },
+    {
+        jadwal_id: "5",
+        dermaga_keberangkatan: "Merak",
+        dermaga_tujuan: "Bakauheni",
+        kelas_layanan: "Express", 
+        tanggal_masuk: "Minggu, 14 Maret 2022",
+        jam_masuk: "06.00 WIB"
+    },
+];
+
+export { JADWAL }
